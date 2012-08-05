603d4604e3396a70b0b37356fad300501addcf90
Say <120011676@qq.com>
2012-08-05 21:53:36 +0800

    test

--- snow/src/test/java/org/qq120011676/snow/util/SqlXmlTest.java
+++ snow/src/test/java/org/qq120011676/snow/util/SqlXmlTest.java
@@ -33,9 +33,9 @@ public class SqlXmlTest extends BaseJunit {
 		// }
 		// Map<String, Object> map = new HashMap<String, Object>();
 		// map.put("c", "");
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("phoneCall", "%" + "" + "%");
-		System.out.println(SqlUtils.getSql("queryTicket", map));
+//		Map<String, Object> map = new HashMap<String, Object>();
+//		map.put("phoneCall", "%" + "" + "%");
+//		System.out.println(SqlUtils.getSql("queryTicket", map));
 
 		// StringBuilder as= new StringBuilder("0123");
 		// String aa ="12";
@@ -44,124 +44,124 @@ public class SqlXmlTest extends BaseJunit {
 		// System.out.println(as);
 	}
 
-	@Test
-	public void dom4j() throws DocumentException {
-		File[] files = new File("E:\\test").listFiles(new SqlFileFilter());
-		SAXReader saxReader = new SAXReader();
-		Document document = saxReader.read(files[0]);
-		Element root = document.getRootElement();
-		if ("sqls".equals(root.getName())) {
-			List<Element> elements = root.elements();
-			for (Element element : elements) {
-				if ("sql".equals(element.getName())) {
-					List<Object> list = element.content();
-					for (int i = 0; i < list.size(); i++) {
-						if (list.get(i) instanceof DefaultText) {
-							System.out.println(((DefaultText) list.get(i))
-									.getText());
-							System.out.println("======");
-						} else if (list.get(i) instanceof Element) {
-							Element element2 = (Element) list.get(i);
-							System.out.println(element2);
-						}
-					}
-				}
-			}
-		}
-	}
-
-	@Test
-	public void sql() throws DocumentException {
-		// File[] files = new File("E:\\test").listFiles(new SqlFileFilter());
-		// SAXReader saxReader = new SAXReader();
-		// Document document = saxReader.read(files[0]);
-		// Element root = document.getRootElement();
-		// this.dom4j(root);
-
-		System.out.println(SqlUtils.getSql("queryCategoryDetails"));
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("detailsName", "");
-		System.out.println(SqlUtils.getSql("queryCategoryDetails", map));
-	}
-
-	public static void main(String[] args) {
-		File[] files = new File("E:\\test").listFiles(new SqlFileFilter());
-		System.out.println(files[files.length-1]);
-		Thread thread = null;
-		if (files != null && files.length >= 0) {
-			for (File file : files) {
-				try {
-					thread = new Thread(new SqlXmlParse(new SAXReader().read(
-							file).getRootElement()));
-					thread.start();
-				} catch (DocumentException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		try {
-			thread.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		System.out.println(new Date().getTime());
-		System.out.println(SqlUtils.getSql("getLoginUser"));
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("detailsName", "");
-		System.out.println(SqlUtils.getSql("queryCategoryDetails", map));
-	}
-
-	public void dom4j(Element element) {
-		if ("sql".equals(element.getName())) {
-			SqlEntity sqlEntity = new SqlEntity();
-			List list = element.content();
-			StringBuilder sqlStringBuilder = new StringBuilder();
-			Map<String, String> mapParameter = new HashMap<String, String>();
-			for (int i = 0; i < list.size(); i++) {
-				if (list.get(i) instanceof DefaultText) {
-					sqlStringBuilder.append(((DefaultText) list.get(i))
-							.getText());
-				} else if (list.get(i) instanceof Element) {
-					Element elementParameter = (Element) list.get(i);
-					if ("parameter".equals(elementParameter.getName())) {
-						String parameterName = elementParameter.attributeValue(
-								"name").trim();
-						sqlStringBuilder.append("${").append(parameterName)
-								.append("}");
-						if (mapParameter.containsKey(parameterName)) {
-							throw new RuntimeException(
-									"this sql parameter name '" + parameterName
-											+ "' already exists!");
-						}
-						mapParameter.put(parameterName,
-								elementParameter.getTextTrim());
-					}
-				}
-				sqlStringBuilder.append(" ");
-			}
-			sqlEntity.setSql(SqlUtils.sqlFormat(sqlStringBuilder.toString()));
-			sqlEntity.setParameters(mapParameter);
-			String type = element.attributeValue("type");
-			if ("sql".equals(type)) {
-				sqlEntity.setSqlType(SqlType.SQL);
-			} else if ("hql".equals(type)) {
-				sqlEntity.setSqlType(SqlType.HQL);
-			} else {
-				sqlEntity.setSqlType(SqlType.SQL);
-			}
-			String name = element.attributeValue("name").trim();
-			if (SqlUtils.getSQLMap().containsKey(name)) {
-				throw new RuntimeException("this sql name '" + name
-						+ "' already exists!");
-			}
-			SqlUtils.getSQLMap().put(name, sqlEntity);
-		} else if ("sqls".equals(element.getName())) {
-			List elements = element.elements();
-			for (Object object : elements) {
-				if (object instanceof Element) {
-					this.dom4j((Element) object);
-				}
-			}
-		}
-	}
+//	@Test
+//	public void dom4j() throws DocumentException {
+//		File[] files = new File("E:\\test").listFiles(new SqlFileFilter());
+//		SAXReader saxReader = new SAXReader();
+//		Document document = saxReader.read(files[0]);
+//		Element root = document.getRootElement();
+//		if ("sqls".equals(root.getName())) {
+//			List<Element> elements = root.elements();
+//			for (Element element : elements) {
+//				if ("sql".equals(element.getName())) {
+//					List<Object> list = element.content();
+//					for (int i = 0; i < list.size(); i++) {
+//						if (list.get(i) instanceof DefaultText) {
+//							System.out.println(((DefaultText) list.get(i))
+//									.getText());
+//							System.out.println("======");
+//						} else if (list.get(i) instanceof Element) {
+//							Element element2 = (Element) list.get(i);
+//							System.out.println(element2);
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
+//
+//	@Test
+//	public void sql() throws DocumentException {
+//		// File[] files = new File("E:\\test").listFiles(new SqlFileFilter());
+//		// SAXReader saxReader = new SAXReader();
+//		// Document document = saxReader.read(files[0]);
+//		// Element root = document.getRootElement();
+//		// this.dom4j(root);
+//
+//		System.out.println(SqlUtils.getSql("queryCategoryDetails"));
+//		Map<String, Object> map = new HashMap<String, Object>();
+//		map.put("detailsName", "");
+//		System.out.println(SqlUtils.getSql("queryCategoryDetails", map));
+//	}
+//
+//	public static void main(String[] args) {
+//		File[] files = new File("E:\\test").listFiles(new SqlFileFilter());
+//		System.out.println(files[files.length-1]);
+//		Thread thread = null;
+//		if (files != null && files.length >= 0) {
+//			for (File file : files) {
+//				try {
+//					thread = new Thread(new SqlXmlParse(new SAXReader().read(
+//							file).getRootElement()));
+//					thread.start();
+//				} catch (DocumentException e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		}
+//		try {
+//			thread.join();
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//		System.out.println(new Date().getTime());
+//		System.out.println(SqlUtils.getSql("getLoginUser"));
+//		Map<String, Object> map = new HashMap<String, Object>();
+//		map.put("detailsName", "");
+//		System.out.println(SqlUtils.getSql("queryCategoryDetails", map));
+//	}
+//
+//	public void dom4j(Element element) {
+//		if ("sql".equals(element.getName())) {
+//			SqlEntity sqlEntity = new SqlEntity();
+//			List list = element.content();
+//			StringBuilder sqlStringBuilder = new StringBuilder();
+//			Map<String, String> mapParameter = new HashMap<String, String>();
+//			for (int i = 0; i < list.size(); i++) {
+//				if (list.get(i) instanceof DefaultText) {
+//					sqlStringBuilder.append(((DefaultText) list.get(i))
+//							.getText());
+//				} else if (list.get(i) instanceof Element) {
+//					Element elementParameter = (Element) list.get(i);
+//					if ("parameter".equals(elementParameter.getName())) {
+//						String parameterName = elementParameter.attributeValue(
+//								"name").trim();
+//						sqlStringBuilder.append("${").append(parameterName)
+//								.append("}");
+//						if (mapParameter.containsKey(parameterName)) {
+//							throw new RuntimeException(
+//									"this sql parameter name '" + parameterName
+//											+ "' already exists!");
+//						}
+//						mapParameter.put(parameterName,
+//								elementParameter.getTextTrim());
+//					}
+//				}
+//				sqlStringBuilder.append(" ");
+//			}
+//			sqlEntity.setSql(SqlUtils.sqlFormat(sqlStringBuilder.toString()));
+//			sqlEntity.setParameters(mapParameter);
+//			String type = element.attributeValue("type");
+//			if ("sql".equals(type)) {
+//				sqlEntity.setSqlType(SqlType.SQL);
+//			} else if ("hql".equals(type)) {
+//				sqlEntity.setSqlType(SqlType.HQL);
+//			} else {
+//				sqlEntity.setSqlType(SqlType.SQL);
+//			}
+//			String name = element.attributeValue("name").trim();
+//			if (SqlUtils.getSQLMap().containsKey(name)) {
+//				throw new RuntimeException("this sql name '" + name
+//						+ "' already exists!");
+//			}
+//			SqlUtils.getSQLMap().put(name, sqlEntity);
+//		} else if ("sqls".equals(element.getName())) {
+//			List elements = element.elements();
+//			for (Object object : elements) {
+//				if (object instanceof Element) {
+//					this.dom4j((Element) object);
+//				}
+//			}
+//		}
+//	}
 }
