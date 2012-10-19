cb42b9b0944e5e2e9acd5a7c6539a111b4bd2b12
Say <120011676@qq.com>
2012-10-19 16:42:53 +0800

    update

---
+++ snow/src/main/java/org/qq120011676/snow/entity/BaseEntity.java
@@ -0,0 +1,15 @@
+package org.qq120011676.snow.entity;
+
+public class BaseEntity {
+
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+}
